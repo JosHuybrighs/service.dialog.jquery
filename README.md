499f0b99c48af263ef2e13c78a96075095beb54d
Jos Huybrighs <jhuybrighs@hotmail.com>
2014-11-08 11:25:08 +0100

    Initial commit of a simple jquery dialog plugin

---
+++ README.md
@@ -0,0 +1,18 @@
+##service.dialog.jquery
+
+###Description
+
+`service.dialog.jquery` is yet another jquery component to bring up a dialog window. I know there are lots of those dialog plugins and so you probably wonder why this one. Well, for me a couple of things are important for such plugins (and too some extend for almost every other type of plugin):
+
+- They have to be small (this one is 1.6 kB minified).
+- They must help me with providing the (software) logic for the problem at hand and not too much the UI side of things; there is often better technology, i.e. HTML and CSS, to solve that than Javascript.
+- The component should therefore not do any html markup inside the dialog at all and neither any styling (yes I want to specify the 'close' button myself).
+- It must be possible to setup the dialog in either modal or non-modal mode.
+
+By now (and also when you look to the other jquery components I developped) you will probably realize that I am not a particular fan of jQuery UI. I find all of this too big, too much for what I typically need, and not particularly friendly with respect to styling.
+
+So, that's why there is this plugin.
+
+###Tutorial and Documentation
+
+You can find the tutorial and API description at [code.cwwonline.be](http://code.cwwonline.be/servicedialogjquery).
---
+++ src/service.dialog.jquery.js
@@ -0,0 +1,163 @@
+/****************************************************************************************************** 
+ * A jquery plugin implementing a modal dialog.
+ * Based almost entirely on Avgrund, created by Hakim El Hattab, http://hakim.se
+ * 
+ * Usage:
+ *  - Instantiation:
+ *    1. Simple (dialog opens immediately) 
+ *      $('#popup').dialog();
+ *       ...
+ *      // Close
+ *      $('#popup').dialog('close');
+ *
+ *    2. Open through seperate command 
+ *      $('#popup').dialog({ openOnInit: false });
+ *       ...
+ *      // Open
+ *      $('#popup').dialog('open');
+ *       ...
+ *      // Close
+ *      $('#popup').dialog('close');
+ *
+ *    2. With open/close callbacks 
+ *      $('#popup').dialog({ onOpen: function () { ... },
+ *                           onClose: function () { ... }
+ *       ...
+ *      // Close
+ *      $('#popup').dialog('close');
+ *      });
+ *
+ * version 1.0.0
+ *
+ * @requires jQuery 1.8.0 or later
+ *
+ * Copyright (c) Jos Huybrighs
+ * code.cwwonline.be
+ *
+ * Licensed under the MIT license.
+ * http://en.wikipedia.org/wiki/MIT_License
+ *
+ ******************************************************************************************************/
+
+; (function ($, win, document, undefined) {
+
+    var pluginName = 'dialog';
+
+    function Plugin(element, options) {
+        // Get the main element
+        this.$element = $(element);
+        // Initialize plugin
+        this._init(options);
+        // Show dialog when told so
+        if (this.settings.openOnInit) {
+            this._activate();
+        }
+    };
+
+    Plugin.prototype = {
+
+        // Activate dialog
+        _activate: function () {
+            this.$element.addClass('dialogOpen');
+            var body = $('body');
+            if (this.settings.modal) {
+                body.addClass('dialogModal');
+                if (this.settings.modalWithOverlay) {
+                    body.addClass('dialogOverlay');
+                }
+                body.append('<div class="dialogBlockPage"></div>');
+            }
+            else {
+                this.$element.addClass('dialogNonModal');
+            }
+            if (this.settings.closeByEscape) {
+                var self = this;
+                body.on('keyup.dialog', function (e) {
+                    if (e.keyCode === 27) {
+                        self._deactivate();
+                    }
+                });
+            }
+            this.state = 'open';
+            // Issue callback
+            this.settings.onOpen();
+        },
+
+        // Deactivatedialog
+        _deactivate: function() {
+            // Issue callback
+            this.settings.onClose();
+            var body = $('body');
+            if (this.settings.closeByEscape) {
+                body.off('.dialog');
+            }
+            this.$element.removeClass('dialogOpen');
+            if (this.settings.modal) {
+                $('.dialogBlockPage').remove();
+                if (this.settings.modalWithOverlay) {
+                    body.removeClass('dialogOverlay');
+                }
+                body.removeClass('dialogModal');
+            }
+            else {
+                this.$element.removeClass('dialogNonModal');
+            }
+            this.state = 'closed';
+        },
+
+        // Initialize
+        _init: function (options) {
+            var self = this;
+            var defaults =
+            {
+                openOnInit: true,
+                closeByEscape: true,
+                modal: true,
+                modalWithOverlay: true,
+                onOpen: function () { },
+                onClose: function () { }
+            };
+            this.settings = $.extend(defaults, options || {});
+            this.state = 'closed';
+        },
+
+        // Close dialog
+        // Arguments: none
+        open: function (argsArray) {
+            this._activate();
+        },
+
+        // Close dialog
+        // Arguments: none
+        close: function (argsArray) {
+            this._deactivate();
+        },
+
+        // isopen: Returns true/false depending on whether the dialog is open or closed
+        // Arguments: none
+        isopen: function (argsArray) {
+            return (this.state == 'open');
+        }
+
+    };
+
+    $.fn[pluginName] = function (methodOrOptions) {
+        var instance = $(this).data(pluginName);
+        if (instance &&
+            methodOrOptions &&
+            typeof methodOrOptions != 'object' &&
+            methodOrOptions.indexOf('_') != 0) {
+            // Call method and return possible result value of the method
+            return instance[methodOrOptions](Array.prototype.slice.call(arguments, 1));
+        }
+        if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+            instance = new Plugin(this, methodOrOptions);
+            $(this).data(pluginName, instance);
+            return this;
+        }
+        $.error('Wrong call to ' + pluginName);
+        return this;
+    };
+
+})(jQuery);
+
